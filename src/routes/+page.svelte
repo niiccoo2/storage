efb0cbe477c7da3d501b51d53e84a5fce37f41a4
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-08-07 16:11:20 -0400

    added stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style>
+    .title_wrapper {
+    position: relative;
+    width: 100%;
+    height: auto;
+    }
+
+    .title_top {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 2;
+    color: white;
+    font-size: 2rem;
+    text-align: center;
+    padding: 0.5em;
+    text-shadow: 1px 1px 3px black;
+    }
+
+</style>
+
+<div>
+    <h1 class="title_top">Welcome to SvelteKit</h1>
+</div>
